817fa6c120ba62d2441c0a828ebcf947005afafb
Carlos Eduardo Teixeira Aguilar <taguilar2504@gmail.com>
2022-09-19 15:29:32 -0300

    Implementando movimentos possíveis da torre

--- Projeto-Xadrez/src/chess/ChessMatch.java
+++ Projeto-Xadrez/src/chess/ChessMatch.java
@@ -32,6 +32,7 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 
 		Piece capturedPiece = makeMove(source, target);
 
@@ -48,6 +49,14 @@ public class ChessMatch {
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to taget position");
+		}
+		
 	}
 
 	private Piece makeMove(Position source, Position target) {
